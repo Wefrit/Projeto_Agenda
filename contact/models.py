322b85ef2d4e6d50a5cf03435e35f6f5e5ba47ea
Wefrit <lopes.nathanlemes@gmail.com>
2026-06-11 11:36:10 -0300

    Cria package de views para organização e gera contatos com uso de faker para teste de templates

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
     name = models.CharField(max_length=50)
 
     def __str__(self)  ->  str:
@@ -23,6 +27,10 @@ class Contact(models.Model):
                                   on_delete=models.SET_NULL,
                                   blank=True, 
                                   null=True,)
+    owner = models.ForeignKey(User,
+                            on_delete=models.SET_NULL,
+                            blank=True, 
+                            null=True,)
 
     def __str__(self)  ->  str:
         return f'{self.first_name} {self.last_name}'
