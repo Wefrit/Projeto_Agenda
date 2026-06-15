b70aed04360ce0323c7c6787c2a5b282fa1d18f4
Wefrit <lopes.nathanlemes@gmail.com>
2026-06-15 11:20:58 -0300

    Adicionando filtro com Q e OR para o campo de busca

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 
@@ -17,6 +18,36 @@ def index(request):
         'contact/index.html',
         context,
     )
+
+
+def search(request):
+
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+                Q(first_name__icontains=search_value) |
+                Q(last_name__icontains=search_value) |
+                Q(phone__icontains=search_value) |
+                Q(email__icontains=search_value)
+            )\
+        .order_by('-id')[:10]\
+    
+    context = {
+        'contacts': contacts,
+        'site_title':'Busca - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
 
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
