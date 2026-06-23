ccd034790a0729093ad054e6e59624cdf7f36602
Wefrit <lopes.nathanlemes@gmail.com>
2026-06-23 13:33:44 -0300

    Usando instance do ModelForm para atualizar dados de um contato

--- contact/urls.py
+++ contact/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     # CRUD 
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/create/', views.create, name='create'),
 ]
