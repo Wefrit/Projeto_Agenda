045fde602caac565eccc3c88a688dfa1e43d7565
Wefrit <lopes.nathanlemes@gmail.com>
2026-06-24 14:34:21 -0300

    Redireciona para página inicial após criar usuário e emite mensagem usando odjango.contrib messages

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from contact.forms import RegisterForm
+from django.contrib import messages
 
 
 def register(request):
@@ -10,6 +11,9 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request,'Usuário registrado')
+            return redirect('contact:index')
+
 
     return render(
         request,
